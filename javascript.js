569c2ccea61569c4a1d4a5a9d0d1c12cfc7b0175
Austin T <austin.taranto@gmail.com>
2020-01-12 15:58:21 -0500

    toggle play function and a few event listeners

--- javascript.js
+++ javascript.js
@@ -8,6 +8,19 @@ const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 // Build out functions 
+function togglePLay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+// function togglePlay() {
+//     const method = video.paused ? "play" : "pause";
+//     video[method]();
+// }
+
 
 // Hook up the eventlisteners 
-
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
